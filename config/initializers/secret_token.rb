941bc1f8ca5d4f9457a9a91218ced25a13c9b4dd
Jasmin Besic <jasmin@optyn.com>
2013-02-12 13:25:16 -0600

    Initial Commit
    First App called jasmins_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasminsApp::Application.config.secret_token = '514676247e846f38515447c8d121953bc464629b645b576ff057e8e9f4e362184b746d32da9438d59a0888ae872408c3a39f2a1eea22dad27f3daaae2efd6166'
